6e8739471fbb7de25530c6f7952551ef72832df1
Naim Sulejmani <naim.sulejmani@gmail.com>
2023-11-23 23:27:05 +0100

    math examples

---
+++ src/other_operators/MathExample.java
@@ -0,0 +1,33 @@
+package other_operators;
+
+public class MathExample {
+    public static void main(String[] args) {
+        System.out.println(Math.min(2, 3)); // minimumi i dy numrave
+        System.out.println(Math.max(45f, 301)); // maximumi i dy numrave
+        System.out.println(Math.abs(-45)); //vlera absolute
+        System.out.println(Math.pow(10, 2)); // 10^2 = 100 // fuqia
+        System.out.println(Math.sqrt(9)); // rrenja katrore
+        System.out.println(Math.random()); // [0, 1) ose >=0 dhe <1
+        System.out.println(Math.round(3.3)); // rrumbullaksimi
+        System.out.println(Math.round(3.7));
+        System.out.println(Math.ceil(2.01));
+        System.out.println(Math.floor(2.99));
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
